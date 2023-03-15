c73b6f1d93e96cb77cdae29524e19312b5b21078
Elsti97 <elsti.shehati@hotmail.it>
2023-03-15 17:31:48 +0100

    Esercizio base fatto

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+        string[] cibiPreferiti = { "Pizza", "Piada Romagnola", "Lasagne", "Sushi", "Cacio e Pepe" };
+
+        Console.WriteLine("La lunghezza della classifica è: " + cibiPreferiti.Length);
+
+    Console.WriteLine("La classifica è:");
+    for (int i = 0; i < cibiPreferiti.Length; i++)
+    {
+        Console.WriteLine((i + 1) + ")" + cibiPreferiti[i]);
+    }
+
+    Console.WriteLine("Il cibo top è: " + cibiPreferiti[0]);
+
+    Console.WriteLine("Il cibo flop è: " + cibiPreferiti[cibiPreferiti.Length - 1]);
+
+    Console.ReadLine();
